3f4ff68243980bb3f7bd7ccc1449788ae6f3a792
Sander Mertens <sander.mertens8@gmail.com>
2017-07-30 02:08:04 -0700

    Copy list members by value

--- api/src/apicollection.c
+++ api/src/apicollection.c
@@ -310,7 +310,14 @@ static corto_int16 c_apiListTypeInsertNoAlloc(corto_list o, corto_string operati
             g_fileWrite(data->source, "%s(list, (void*)element);\n", corto_operationToApi(operation, api));
             g_fileWrite(data->source, "}\n");
         } else {
-            g_fileWrite(data->source, "%s(list, (void*)(corto_word)element);\n", corto_operationToApi(operation, api));
+            if (elementType->kind == CORTO_PRIMITIVE) {
+                g_fileWrite(data->source, "%s(list, (void*)(corto_word)element);\n", corto_operationToApi(operation, api));
+            } else {
+                g_fileWrite(data->source, "%s%s result;\n", elementId, ptr ? "*" : "");
+                g_fileWrite(data->source, "corto_ptr_init(&result, %s);\n", varId);
+                g_fileWrite(data->source, "corto_ptr_copy(&result, %s, %selement);\n", varId, ptr ? "" : "&");
+                g_fileWrite(data->source, "%s(list, result);\n", corto_operationToApi(operation, api));
+            }
         }
         if (elementType->reference) {
             g_fileWrite(data->source, "if (element) {\n");
