a6717ea15919657ec37dc9d75c2a68549078b1ec
Sander Mertens <sander.mertens8@gmail.com>
2017-12-09 17:30:12 -0800

    Change corto_proccmd to corto_proc_cmd

--- api/src/api.c
+++ api/src/api.c
@@ -354,7 +354,7 @@ corto_int16 genmain(g_generator g) {
             corto_int8 ret, sig;
             corto_id cmd;
             sprintf(cmd, "corto create package %s/c --unmanaged --notest --nobuild --silent -o c", g_getName(g));
-            sig = corto_proccmd(cmd, &ret);
+            sig = corto_proc_cmd(cmd, &ret);
             if (sig || ret) {
                 corto_throw("failed to setup project for '%s/c'", g_getName(g));
                 goto error;
